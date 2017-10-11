0832bb43a732c509953452d93a6103d7ec89423e
a.gurianov <a.gurianov@mac517rih.rambler.ramblermedia.com>
2017-10-11 11:37:08 +0300

    Changed setup.py and top level __init__.py to allow import directly from spectrum Several bugfixes in bispectrumi function.

--- setup.py
+++ setup.py
@@ -7,15 +7,7 @@ setup(
   name = "spectrum",
   version = "0.1",
   packages = find_packages(),
-  # scripts = ['say_hello.py'],
-
   install_requires = ['numpy>=1.8', 'scipy>0.13.3', 'matplotlib>1.3.1'],
-
-  include_package_data = True,
-
-  package_data = {
-    '': ['*.txt', '*.rst', '*.mat'],
-  },
 
   # metadata for upload to PyPI
   author = "ixaxaar",
